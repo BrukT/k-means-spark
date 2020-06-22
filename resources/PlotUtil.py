26069b1621559d1789b852f49b25dc40dca3257e
BrukT <brukteka@gmail.com>
2020-06-22 12:27:23 +0200

    removed some for loop to optimize performance

--- resources/PlotUtil.py
+++ resources/PlotUtil.py
@@ -2,6 +2,7 @@
 This is a Plot utility class used for plotting the result if the code is running in the local mode.
 """
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class PLotUtil:
@@ -9,14 +10,19 @@ class PLotUtil:
     @staticmethod
     def plot_list(points, col='blue', mark='o', sz=20):
         # plotting list of points
-        x, y = [], []
-        for pt in points:
-            x.append(pt[0])
-            y.append(pt[1])
+        points = np.asarray(points)
+        x = points[:, 0]
+        y = points[:, 1]
         plt.scatter(x, y, color=col, marker=mark, s=sz)
 
     @staticmethod
     def clustering_plot(points, means, closest_mean):
+        '''
+        points = np.asarray(po)
+        color = 'red' if closest_mean(points, means) == 0 else \
+            'blue' if closest_mean(points, means) == 1 else \
+            'green' if closest_mean(points, means) == 2 else \
+            'yellow' if closest_mean(points, means) == 3 else None '''
         for pt in points:
             if closest_mean(pt, means) == 0:
                 plt.scatter(pt[0], pt[1], color='red')
@@ -26,6 +32,7 @@ class PLotUtil:
                 plt.scatter(pt[0], pt[1], color='green')
             elif closest_mean(pt, means) == 3:
                 plt.scatter(pt[0], pt[1], color='yellow')
+
         PLotUtil.plot_list(means, col='black', mark='*', sz=400)
         plt.show()
 
